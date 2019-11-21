2b93573a3f6c09198b5737556a3e7870ac628932
Arthur Lee <ruhtraeel@gmail.com>
2019-11-20 17:38:38 -0800

    Chapter 26 Lesson 9: Sending Http Requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,7 +8,18 @@ function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
+    fetch('https://react-hooks-update-10777.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        {id: responseData.name, ...ingredient}
+      ]);
+    });
   };
 
   return (
