07338269753a1b0aaac728eb4e9477e4406d1be4
Arthur Lee <ruhtraeel@gmail.com>
2019-11-22 16:39:23 -0800

    Chapter 26 Lesson 14: Working with Refs & useRef

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,30 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
-  const {onLoadIngredients} = props;
+  const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ?
-    '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-update-10777.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
+    setTimeout(() => {
+      // Enteredfilter will be the old enteredFilter the user entered 500 ms ago
+      // inputRef.current.value = current value of the enteredFilter
+      if (enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 ?
+          '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-update-10777.firebaseio.com/ingredients.json' + query)
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
       }
-      onLoadIngredients(loadedIngredients);
-    });
+    }, 500);
+
     // Array element = If any of the elements in the array change, then the useEffect runs again
-  }, [enteredFilter, onLoadIngredients]);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -32,6 +40,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)}
