6cea912204fb2b24bf0c4b6e0dfd4da1bdb916f4
Arthur Lee <ruhtraeel@gmail.com>
2019-11-24 14:23:45 -0800

    Chapter 26 Lesson 20: Understanding useReducer()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,26 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useReducer, useState, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get here!');
+  }
+}
+
 function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  //const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -14,8 +28,9 @@ function Ingredients() {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
-  const filteredIngredientsHandler = useCallback(filterIngredients => {
-    setUserIngredients(filterIngredients);
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    //setUserIngredients(filteredIngredients);
+    dispatch({type: 'SET', ingredients: filteredIngredients});
   }, []);
 
   const addIngredientHandler = ingredient => {
@@ -28,10 +43,14 @@ function Ingredients() {
       setIsLoading(false);
       return response.json();
     }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        {id: responseData.name, ...ingredient}
-      ]);
+      //setUserIngredients(prevIngredients => [
+      //  ...prevIngredients,
+      //  {id: responseData.name, ...ingredient}
+      //]);
+      dispatch({
+        type: 'ADD',
+        ingredient: {id: responseData.name, ...ingredient}
+      });
     });
   };
 
@@ -41,8 +60,9 @@ function Ingredients() {
       method: 'DELETE'
     }).then(response => {
       setIsLoading(false);
-      setUserIngredients(prevIngredients =>
-        prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+      //setUserIngredients(prevIngredients =>
+      //  prevIngredients.filter(ingredient => ingredient.id !== ingredientId));
+      dispatch({type: 'DELETE', id: ingredientId});
     }).catch(error => {
       // Both of these state updates are done together (only 1 render cycle that reflects both state updates)
       setError('Something went wrong!');
